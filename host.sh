8faf27386e897088d10fd74103c2ae002af905cb
Khadimou <khadimou-rassoul.diop@ens.uvsq.fr>
2023-02-03 09:40:45 +0100

    ping sur windows avec l'option -n

--- host.sh
+++ host.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-ipadrr='172.31.96'
+ipadrr='172.17.12'
 let "connected= 0"
 let "notconnected= 1"
 for ip in $ipadrr.{1..255}
 do
-	if [ "`ping -c 1 ip &> /dev/null`" ]; then
+	ping -n 1 "$ip" > /dev/null
+	if [ $? -eq 0 ] > /dev/null; then
 		echo "$ip est connected, exit $?"
 		let "connected = connected + 1"
 	else
